9aa464cc30520c9eef7689dffd000a349cb10eb5
KAR56 <56875891+KAR56@users.noreply.github.com>
2019-10-23 21:52:21 -0400

    Update turtleString.py

--- turtleString.py
+++ turtleString.py
@@ -1,5 +1,5 @@
-#Modified by:  Kyle Rose
-#Email:     Kyle.Rose28@myhunter.cuny.edu
+#Modified by: !Your Name Here!
+#Email:    !Your Email Here!
 #A program that uses command strings to control turtle drawing
 
 
@@ -15,14 +15,8 @@ for ch in commands:
         tess.forward(50)
     elif ch == 'L':          #turn left
         tess.left(90)
-    elif ch == 'T':
-        tess.left(45)
     elif ch == 'R':          #turn right
         tess.right(90)
-    elif ch == 'r':
-        tess.right(45)
-    elif ch == 'S':
-        tess.stamp()
     elif ch == '^':          #lift pen
         tess.penup()
     elif ch == 'v':          #lower pen
@@ -33,8 +27,6 @@ for ch in commands:
         tess.color("green")
     elif ch == 'b':          #turn blue
         tess.color("blue")
-    elif ch == 'p':
-        tess.color("purple")
     else:                   #for any other character, print an error message
         print("Error: do not know the command:", c)
 
